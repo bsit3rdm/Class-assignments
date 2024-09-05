f419ceaf89a39c6a4d2648a4260d4c963ccd0959
Bsit 3rd-Morning <bsitrdsemester@gmail.com>
2024-09-05 09:45:07 -0700

    CGPA.cpp
    CGPA calculator

---
+++ CGPA .cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+int numsubject;
+float gradepoints, credithours, CGPA, totalgradepoints=0.0, totalcredithours=0.0;
+
+cout<<"Enter the no of subjects="<<endl;
+cin>>numsubject;
+for(int i=0;i<numsubject;i++) {
+   cout<<"enter gradepoints for subject"<<i+1<<":"<<endl;
+   cin>>gradepoints;
+   cout<<"enter credithours for subject"<<i+1<<":"<<endl;
+   cin>>credithours;
+ totalgradepoints+=gradepoints*credithours;
+ totalcredithours+=credithours;
+}
+
+CGPA=(totalgradepoints)/(totalcredithours);
+cout<<"The CGPA of the student
+
+ is ="<<CGPA<<endl;
+return 0;
+}
