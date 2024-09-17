85ae9b3d7e542383b5b3198b316f23186d4d81cb
Bsit 3rd-Morning <bsitrdsemester@gmail.com>
2024-09-17 22:32:04 +0500

    CGPA-calculator.cpp

---
+++ BSF23002102-Asadullah Khan/CGPA-calculator.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+int creditHours,totalCrHr=0;
+	float credits, totalCredits=0;
+	int marks;
+	float gp,gpa;
+	void subjViseCredits()
+	{
+		cout << "Enter your marks: ";
+		cin >> marks;
+		cout << "Enter your credit hours: ";
+		cin >> creditHours;
+		if (marks >= 50 && marks <= 90)
+		{
+			gp = (marks - 50) * 0.05 + 2;
+			credits = gp * creditHours;
+			
+		}
+		else if(marks>90 && marks<=100)
+		{
+			credits=4*creditHours;
+		}
+		else if(marks<50 && marks>=0)
+		{
+			cout<<"You failed this subject!" << endl;
+		}
+			totalCredits+=credits;
+			totalCrHr+=creditHours;
+	}
+int main()
+{
+	int i,subjects;
+	cout<<"Enter The number of subjects: ";
+	cin>>subjects;	
+	for( i=1; i<=subjects; i++)
+	{
+		cout<<"Enter Marks for subj "<<i<<endl;
+		subjViseCredits();
+	}
+		gpa=totalCredits/totalCrHr;
+		cout<<"GPA: "<<gpa;
+	return 0;
+}
